bf204ccb9c3aa3e8932f91f9fd54bb2b3d0878d4
Alex Hayes <alex@alution.com>
2011-05-20 18:38:47 +1000

    Added compass config.rb and generated stylesheet

---
+++ config.rb
@@ -0,0 +1,10 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "sass"
+images_dir = "assets/images"
+javascripts_dir = "assets/js"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+relative_assets = true
