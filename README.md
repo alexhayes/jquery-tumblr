609d0e2c104bd85ab9e0567cd7df4dfb17dd70e7
Alex Hayes <alex@alution.com>
2011-06-03 11:42:06 +1000

    Added pagination and loading capabilities. Modified api slightly to use CamelCase throughout.

--- README.md
+++ README.md
@@ -9,14 +9,17 @@ Note that the markup generated is similar to that of tumblr's default theme howe
 ## Example
 
 ```html
+<div id="loading">Loading...</div>
 <div id="posts"></div>
+<div id="pagination"></div>
 	<script type="text/javascript">
 	$(document).ready(function () {
 		$('#posts')
 			.tumblr({
-				url: 'http://blog.thehiddenventure.com'
-			})
-			.tumblr('reload');
+				url:        'http://blog.thehiddenventure.com',
+				pagination: '#pagination',
+				loading:    '#loading'
+			});
 	});
 	</script>
 ```
@@ -26,17 +29,24 @@ Note that the markup generated is similar to that of tumblr's default theme howe
 The following options are available:
 
 - url: your tumblr url (ie. http://(YOUR NAME).tumblr.com )
-- timeout: defaults to 10000
+- loading: A selector that specifies a element that can be shown when loading content from tumblr.
+- pagination: A selector that specifies where the pagination will reside. If one does not exist, pagination is disabled.
 - perPage: The number of posts to return. The default is 20, and the maximum is 50.
 - start: The post offset to start from. The default is 0.
+- paginationOptions: Other options to pass to jquery_pagination - See https://github.com/gbirke/jquery_pagination
 - photoSize: The photo size to use, accepted values are 75, 100, 250, 400, 500 and 1280. Default is 400.
 - videoSize: The video size to embed, accepted values are 250, 500 or false. If false, the tumblr 'video-player' parameter will be used.
 - timeago: If true (default) then jquery-timeago will be used for post dates.
 - shortLength: For captions and titles that are less than this length the css class 'short' will be added. Default is 50.
 - mediumLength: For captions and titles that are less than this length the css class 'medium' will be added. Default is 100.
+- fancybox: If true (default) then fancybox will be used when there are multiple photos. See photoThumbSize and photoLightboxSize
+- photoThumbSize: If there are multiple photos, they will be output in a ul/li tags, this value will be the size of image used. Accepted values are 75, 100, 250, 400, 500 and 1280. Default is 75.
+- photoLightboxSize: If there are multiple photos, this image size will be used for lightbox. Accepted values are 75, 100, 250, 400, 500 and 1280. Default is 1280.
+- timeout: Ajax timeout defaults to 5000
 
 ## Optional Requirements
 
+[jquery_pagination](https://github.com/gbirke/jquery_pagination.git) - is needed if you want pagination. Until an issue with jquery_pagination is resolved then you should use [my fork](https://github.com/alexhayes/jquery_pagination) 
 [jquery-timeago](http://timeago.yarp.com/) - is needed if the timeago option is true (default).
 
 ## License
--- jquery.tumblr.js
+++ jquery.tumblr.js
@@ -20,31 +20,37 @@
 		 * 
 		 * @param options     Valid params:
 		 *                        - url: your tumblr url (ie. http://(YOUR NAME).tumblr.com )
-		 *                        - timeout: defaults to 10000
+		 *                        - loading: A selector that specifies a element that can be shown when loading content from tumblr.
+		 *                        - pagination: A selector that specifies where the pagination will reside. If one does not exist, pagination is disabled.
 		 *                        - perPage: The number of posts to return. The default is 20, and the maximum is 50.
 		 *                        - start: The post offset to start from. The default is 0.
+		 *                        - paginationOptions: Other options to pass to jquery_pagination - See https://github.com/gbirke/jquery_pagination
 		 *                        - photoSize: The photo size to use, accepted values are 75, 100, 250, 400, 500 and 1280. Default is 400.
 		 *                        - videoSize: The video size to embed, accepted values are 250, 500 or false. If false, the tumblr 'video-player' parameter will be used.
 		 * 	                      - timeago: If true (default) then jquery-timeago will be used for post dates.
 		 *                        - shortLength: For captions and titles that are less than this length the css class 'short' will be added. Default is 50.
 		 *                        - mediumLength: For captions and titles that are less than this length the css class 'medium' will be added. Default is 100.
-		 *                        - fancyBox: If true (default) then fancybox will be used when there are multiple photos. See photoThumbSize and photoLightboxSize
+		 *                        - fancybox: If true (default) then fancybox will be used when there are multiple photos. See photoThumbSize and photoLightboxSize
 		 *                        - photoThumbSize: If there are multiple photos, they will be output in a ul/li tags, this value will be the size of image used. Accepted values are 75, 100, 250, 400, 500 and 1280. Default is 75.
 		 *                        - photoLightboxSize: If there are multiple photos, this image size will be used for lightbox. Accepted values are 75, 100, 250, 400, 500 and 1280. Default is 1280.
+		 *                        - timeout: Ajax timeout defaults to 5000
 		 */
 		init : function( options ) {
 			var settings = {
-				'timeout'           : 3000,
-				'perPage'           : 20,
-				'start'             : 0,
-				'photoSize'         : 400,
-				'videoSize'         : false,
-				'timeago'           : true,
-				'shortLength'       : 50,
-				'mediumLength'      : 100,
-				'fancybox'          : true,
+				'loading'             : false,
+				'pagination'          : false,
+				'perPage'            : 20,
+				'start'               : 0,
+				'paginationOptions'  : {},
+				'photoSize'          : 400,
+				'videoSize'          : false,
+				'timeago'             : true,
+				'shortLength'        : 50,
+				'mediumLength'       : 100,
+				'fancybox'            : true,
 				'photoThumbSize'    : 75,
-				'photoLightboxSize' : 500
+				'photoLightboxSize' : 500,
+				'timeout'             : 5000
 		    };
 			var that = this;
 			
@@ -59,25 +65,51 @@
 					if ( options ) { 
 						$.extend( settings, options );
 					}
+					if( settings.pagination ) {
+						settings.pagination = $(settings.pagination);
+						if( settings.pagination.length < 1 ) {
+							settings.pagination = false;
+						}
+					}
+					if( settings.loading ) {
+						settings.loading = $(settings.loading);
+						if( settings.loading.length < 1 ) {
+							settings.loading = false;
+						}
+					}
 					$(this).data('tumblr', {
-						target  : $this,
-						start   : settings.start,
-						options : settings,
-						posts   : posts
+						target           : $this,
+						start            : settings.start,
+						options          : settings,
+						posts            : posts,
+						pagination_setup : false 
 					});
 				}
+				$this.tumblr('load');
 			});
 		},
 		
 		/**
-		 * Reload the tumblr feed.
+		 * Load the tumblr feed.
+		 * 
+		 * @param Int page    The page number to load (starts at 0).
 		 */
-		reload : function(start) {
-			// Only make one ajax call - but load it for all matched elements...
+		load: function(page) {
+
+			// Show all the loaders and slide up the content.
+			this.each(function() {
+				var $this = $(this),
+					data = $this.data('tumblr');
+				if(data.options.loading) {
+					data.options.loading.show();
+				}
+				$this.slideUp();
+			});
+
 			var $this = this, 
 				data = this.data('tumblr'),
 				params = {
-					start: start == undefined ? data.start : start,
+					start: page == undefined ? data.start * data.options.perPage : page * data.options.perPage,
 					num: data.options.perPage
 				},
 				url = data.options.url + '/api/read/json?' + $.param(params);				
@@ -87,10 +119,10 @@
 				dataType: 'script',
 				timeout: data.options.timeout,
 				success: function() {
-					$this.tumblr('handle_ajax_success', tumblr_api_read);
+					$this.tumblr('handleAjaxSuccess', tumblr_api_read);
 				},
 				error: function (xhr, statusTxt, errorTxt) {
-					// Unlikely that this will be supported because in probably 99.9% of cases it will be cross domain.
+					// Unlikely that this will be supported because in probably 99.9% of cases it will be cross domain. Will only work if they have setup a tunnel...
 					$this.append(
 						'<h2>Ooops...</h2>' +
 						'<p>It looks like tumblr is having issues - it happens to the best of us. Don\'t worry it should be fixed soon!</p>' +
@@ -101,20 +133,22 @@
 			return this;
 		},
 		
-		handle_ajax_success: function(tumblr_api_read) {
+		handleAjaxSuccess: function(tumblr_api_read) {
 			return this.each(function() {
 				var $this = $(this),
 					data = $this.data('tumblr'),
+					posts = data.posts,
 					postIterator = 0;
 
+				data.posts.empty();
 				$this.empty();
 				if ((tumblr_api_read == undefined) || (tumblr_api_read == null)) {
 					$this.append('<div class="tumblr-error">Unable to load tumblr - its probably down...</div>');
 					return;
 				}
 				
-				$.each(tumblr_api_read.posts.slice(0, 10), function(i, post) {
-					$this.tumblr('add_post', post, postIterator);
+				$.each(tumblr_api_read.posts, function(i, post) {
+					$this.tumblr('addPost', post, postIterator);
 					postIterator++;
 				});
 				
@@ -124,12 +158,29 @@
 				if(data.options.fancybox && $("a.lightbox", data.posts).length > 0) {
 					$("a.lightbox", data.posts).fancybox();
 				}
-				$this.append(data.posts);
+				$this.html(data.posts);
+				if(data.options.loading) {
+					data.options.loading.hide();
+				}
+				$this.slideDown();
 				
+				if(data.options.pagination && !data.pagination_setup) {
+					data.pagination_setup = true;
+					$.extend(
+						data.options.paginationOptions, 
+						{
+					        items_perPage : data.options.perPage,
+					        callback       : function(new_page_index, pagination_container) {
+					        	$this.tumblr('load', new_page_index);	
+					        }
+						}
+					);
+					data.options.pagination.pagination(tumblr_api_read['posts-total'], data.options.paginationOptions);
+				}
 			});
 		},
 
-		add_post: function(post, i) {
+		addPost: function(post, i) {
 			var $this = $(this),
 				data = $this.data('tumblr'),
 				oddeven = i%2 ? 'even' : 'odd',
@@ -138,7 +189,7 @@
 
 			switch(post.type) {
 				case "regular": {
-					var extraClass = $this.tumblr('get_css_text_length', post['regular-title']);
+					var extraClass = $this.tumblr('getCssTextLength', post['regular-title']);
 					body = '<div class="title ' + extraClass + '">' + post['regular-title'] + '</div>' +
                         	'<div class="description">' + post['regular-body'] + '</div>';
 					break;
@@ -174,7 +225,7 @@
 					break;
 				}
 				case "link": {
-					var extraClass = $this.tumblr('get_css_text_length', post['link-text']);
+					var extraClass = $this.tumblr('getCssTextLength', post['link-text']);
 					body = '<div class="link ' + extraClass + '"><a href="' + post['link-url'] + '">' + post['link-text'] + '</a></div>';
 					if(post["link-description"]) {
 						body += '<div class="description">' + post['link-description'] + '</div>';
@@ -182,7 +233,7 @@
 					break;
 				}
 				case "quote": {
-					var extraClass = $this.tumblr('get_css_text_length', post['quote-text']);
+					var extraClass = $this.tumblr('getCssTextLength', post['quote-text']);
 					body = 
 	                    '<div class="quote">' +
 	                        '<div class="quote-text ' + extraClass + '">' + post['quote-text'] + '</div>' +
@@ -191,7 +242,7 @@
 					break;
 				}
 				case "conversation": {
-					var extraClass = $this.tumblr('get_css_text_length', post['conversation-title']);
+					var extraClass = $this.tumblr('getCssTextLength', post['conversation-title']);
 					body = '<div class="caption ' + extraClass + '">' + post['conversation-title'] + '</div>' +
 						'<div class="conversation">' + '<ul>';
 					
@@ -267,7 +318,7 @@
 			data.posts.append(li);
 		},
 
-		get_css_text_length: function(text) {
+		getCssTextLength: function(text) {
 			var $this = $(this),
 				data = $this.data('tumblr'),
 				shortLength = data.options.shortLength,
@@ -283,7 +334,7 @@
 			return extraClass;
 		},
 
-		destroy : function() {
+		destroy: function() {
 			return this.each(function(){
 				var $this = $(this),
 					data = $this.data('tumblr');
